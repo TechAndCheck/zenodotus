eeba614767390b2a548ef5abcc1172ea01b00615
Christopher Guess <cguess@gmail.com>
2021-03-23 16:37:58 -0400

    Adds validate_url gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
     Bullet.alert         = true
     Bullet.bullet_logger = true
     Bullet.console       = true
-  # Bullet.growl         = true
+    # Bullet.growl         = true
     Bullet.rails_logger  = true
     Bullet.add_footer    = true
   end
