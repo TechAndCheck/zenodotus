80987a1bdd382295f07dbd4ab997db2a13b8efaf
Christopher Guess <cguess@gmail.com>
2021-04-13 10:59:20 -0400

    Add generator for media sources
    This adds a new generator for media sources, allowing
    `rails g media_source test_thing` to create a new media source, along
    with the tests.`

---
+++ lib/generators/media_source/media_source_generator.rb
@@ -0,0 +1,26 @@
+# typed: false
+class MediaSourceGenerator < Rails::Generators::NamedBase
+  desc "This generator creates a new media source for collecting and archiving of content."
+
+  source_root File.expand_path("templates", __dir__)
+
+  TEMPLATE_PATH = "lib/generators/media_source/templates/media_source.rb.erb"
+  TEST_TEMPLATE_PATH = "lib/generators/media_source/templates/media_source_test.rb.erb"
+
+  def copy_media_source_files
+    create_file "app/media_sources/#{file_name}_media_source.rb",
+                generate_template(TEMPLATE_PATH)
+    create_file "test/media_sources/#{file_name}_media_source_test.rb",
+                generate_template(TEST_TEMPLATE_PATH)
+  end
+
+private
+
+  def generate_template(template_path)
+    file_contents = nil
+    File.open(template_path) { |f| file_contents = f.read }
+
+    @file_name = file_name
+    ERB.new(file_contents).result(binding)
+  end
+end
---
+++ test/lib/generators/media_source_generator_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+require "generators/media_source/media_source_generator"
+
+class MediaSourceGeneratorTest < Rails::Generators::TestCase
+  tests MediaSourceGenerator
+  destination Rails.root.join('tmp/generators')
+  setup :prepare_destination
+
+  # test "generator runs without errors" do
+  #   assert_nothing_raised do
+  #     run_generator ["arguments"]
+  #   end
+  # end
+end
