96e3579dfdc22782da8ae5eaa7bb21c426036513
oneroyalace <oneroyalace@gmail.com>
2021-07-20 15:25:11 -0400

    Fix rails model typo (#68)

--- db/migrate/20210628185327_create_twitter_video_preview.rb
+++ db/migrate/20210628185327_create_twitter_video_preview.rb
@@ -3,7 +3,7 @@
 # Update all twitter videos to have a preview image
 class CreateTwitterVideoPreview < ActiveRecord::Migration[6.1]
   def change
-    Source::Tweet.all do |tweet|
+    Sources::Tweet.all do |tweet|
       tweet.videos.each { |video| video.video_derivatives! }
     end
   end
