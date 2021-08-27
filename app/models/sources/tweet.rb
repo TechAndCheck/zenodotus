c1be8337909e56b802e5b80f546bfb74d7f1b067
Asa Royal <oneroyalace@gmail.com>
2021-08-27 14:08:03 -0400

    proper comments

--- app/models/sources/tweet.rb
+++ app/models/sources/tweet.rb
@@ -17,7 +17,6 @@ class Sources::Tweet < ApplicationRecord
     self.videos.each { |video| video.video_derivatives! }
   end
 
-  # TODO: Run materialized view refreshes asynchronously using ActiveJob
   # update materialized view when a new tweet is added
   after_commit on: [:create, :destroy] do
     UnifiedTableRefreshJob.perform_later
