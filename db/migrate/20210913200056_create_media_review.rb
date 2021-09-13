4174fd9ef3eb79f4ae76aac7012231829e4bf6d7
oneroyalace <oneroyalace@gmail.com>
2021-09-13 17:03:42 -0400

    Remove prefix from column names

--- db/migrate/20210913200056_create_media_review.rb
+++ db/migrate/20210913200056_create_media_review.rb
@@ -2,9 +2,9 @@ class CreateMediaReview < ActiveRecord::Migration[6.1]
   def change
     create_table :media_review, id: :uuid do |t|
       t.timestamps
-      t.text :media_link, null: false
-      t.text :media_authenticity, null: false
-      t.text :media_context, null: false
+      t.text :link, null: false
+      t.text :authenticity, null: false
+      t.text :context, null: false
       t.belongs_to :archive_item, type: :uuid, foreign_key: true
       # has_one claimreview?
     end
