105f1db5ba89cf53e02d8a061ef299729bee4408
oneroyalace <oneroyalace@gmail.com>
2021-09-14 09:34:41 -0400

    Correct module path for Tweet

--- test/models/unified_user_test.rb
+++ test/models/unified_user_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 class UnifiedUserTest < ActiveSupport::TestCase
   test "adding_tweet_refreshes_unified_user_view" do
-    Tweet.create_from_url "https://twitter.com/scottwongDC/status/1415040665596084237"
+    Sources::Tweet.create_from_url "https://twitter.com/scottwongDC/status/1415040665596084237"
+    byebug
     assert_equal UnifiedUser.all.length, 1
   end
 end
