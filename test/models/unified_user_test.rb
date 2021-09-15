36ed80488093be82b1856aff5934bd6aad73518d
oneroyalace <oneroyalace@gmail.com>
2021-09-15 10:05:13 -0400

    Run ActiveJobs even for tests that don't inherit from ActiveJob::TestCase

--- test/models/unified_user_test.rb
+++ test/models/unified_user_test.rb
@@ -1,9 +1,14 @@
 require "test_helper"
+# require "ActiveJob::TestHelper::TestQueueAdapter"
 
-class UnifiedUserTest < ActiveSupport::TestCase
+class UnifiedUserTest < ActionController::TestCase
+  include ActiveJob::TestHelper
+
   test "adding_tweet_refreshes_unified_user_view" do
     Sources::Tweet.create_from_url "https://twitter.com/scottwongDC/status/1415040665596084237"
-    byebug
+    assert_enqueued_with(job: UnifiedTableRefreshJob)
+    perform_enqueued_jobs
+    assert_performed_jobs 1
     assert_equal UnifiedUser.all.length, 1
   end
 end
