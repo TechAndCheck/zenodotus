c6d2e8d7d8612dde6fc2ef4f12530dfb14a215d2
oneroyalace <oneroyalace@gmail.com>
2021-10-11 13:21:18 -0400

    correct model relationships

--- app/models/media_review_models/media_review_author.rb
+++ app/models/media_review_models/media_review_author.rb
@@ -1,6 +1,5 @@
 # typed: false
 
 class MediaReviewModels::MediaReviewAuthor < ApplicationRecord
-  belongs_to :media_review, optional: true, class_name: "MediaReviewModels::MediaReview"
-
+  belongs_to :media_review, optional: false, class_name: "MediaReviewModels::MediaReview"
 end
--- app/models/media_review_models/media_review_item.rb
+++ app/models/media_review_models/media_review_item.rb
@@ -1,11 +1,10 @@
 # typed: false
 
 class MediaReviewModels::MediaReviewItem < ApplicationRecord
-  belongs_to :media_review, optional: true, class_name: "MediaReviewModels::MediaReview"
+  belongs_to :media_review, optional: false, class_name: "MediaReviewModels::MediaReview"
 
-  has_one :media_review_item_author
-  has_many :media_item_appearance
+  has_one :media_review_item_author, dependent: :destroy
+  has_many :media_item_appearance, dependent: :destroy
 
   accepts_nested_attributes_for :media_review_item_author, :media_item_appearance
-
 end
