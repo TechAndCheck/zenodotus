c3ea9b405fe6c6eeec17ab358a63ad5ec9d05372
oneroyalace <oneroyalace@gmail.com>
2021-10-12 09:09:35 -0400

    add model dependencies

--- app/models/media_review_models/media_review.rb
+++ app/models/media_review_models/media_review.rb
@@ -1,8 +1,8 @@
 class MediaReviewModels::MediaReview < ApplicationRecord
   belongs_to :archive_item, optional: false, class_name: "ArchiveItem"
 
-  has_one :media_review_item
-  has_one :media_review_author
+  has_one :media_review_item, dependent: :destroy
+  has_one :media_review_author, dependent: :destroy
 
   accepts_nested_attributes_for :media_review_item, :media_review_author
 end
