0b81e63c43e9820595b25ea29a2fcc9bf06e847f
Christopher Guess <cguess@gmail.com>
2021-12-14 16:01:37 +0000

    changes to the schema

--- db/schema.rb
+++ db/schema.rb
@@ -102,10 +102,10 @@ ActiveRecord::Schema.define(version: 2021_09_14_145948) do
   end
 
   create_table "text_searches", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "query"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.uuid "user_id"
+    t.string "query"
+    t.uuid "user_id", null: false
     t.index ["user_id"], name: "index_text_searches_on_user_id"
   end
 
