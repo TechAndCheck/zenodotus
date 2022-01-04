4b9a3e10320763a346586ba9a31fdaf3577ed9aa
oneroyalace <oneroyalace@gmail.com>
2022-01-04 09:57:32 -0500

    implement pr review

--- app/channels/jobs_channel.rb
+++ app/channels/jobs_channel.rb
@@ -4,6 +4,5 @@ class JobsChannel < ApplicationCable::Channel
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
   end
 end
--- test/channels/jobs_channel_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class JobsChannelTest < ActionCable::Channel::TestCase
-  # test "subscribes" do
-  #   subscribe
-  #   assert subscription.confirmed?
-  # end
-end
