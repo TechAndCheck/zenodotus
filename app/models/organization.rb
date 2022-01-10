86fe1d7d43034f49562894bf0a2fddc4541e36b3
Christopher Guess <cguess@gmail.com>
2022-01-10 15:22:52 -0500

    Intial organization views

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,7 +1,6 @@
 class Organization < ApplicationRecord
   # Things this class should do:
   # Send an email to the admin when a user is crud'd
-  # Package all media under it
 
   # This should always be set, but we can't require it because otherwise you get a race condition
   # where the User can't be created without an Organization and an Organization can't be created without
@@ -9,6 +8,7 @@ class Organization < ApplicationRecord
   belongs_to :admin, class_name: :User, required: false
 
   has_many :users, dependent: :destroy
+  has_many :archive_items, through: :users
 
   before_save :ensure_admin_if_users
 
