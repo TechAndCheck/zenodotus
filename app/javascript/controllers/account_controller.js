7807b14e7306004d78451f88c7ae7d0b6ebd694b
oneroyalace <oneroyalace@gmail.com>
2022-01-20 16:16:48 -0500

    move page loading logic to stimulus

--- app/javascript/controllers/account_controller.js
+++ app/javascript/controllers/account_controller.js
@@ -1,8 +1,25 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
+  connect() {
+    const url = window.location.href
+    console.log('load', url)
+    if (url.includes('text_search')) {
+      document.getElementById('accountSettings').hidden = true
+      document.getElementById('textSearchHistory').removeAttribute('hidden')
+      console.log('accountSettings.hidden is now', document.getElementById('accountSettings').hidden)
+      console.log('textSearchHistory.hidden is now', document.getElementById('textSearchHistory').hidden)
+      currentView = "textSearchHistory"
+    }
+    else if (url.includes("image_search")) {
+      document.getElementById("accountSettings").hidden = true;
+      document.getElementById("imageSearchHistory").removeAttribute("hidden");
+      currentView = "imageSearchHistory";
+    }
+  }
+
   /**
-   *
+   * Toggle the section visible on the accounts page
    */
   toggleView({ params: { id } }) {
     const currentView = [...document.getElementsByClassName('settingsBlock')].filter((i) => i.getAttribute('hidden') == null)[0]
@@ -10,7 +27,7 @@ export default class extends Controller {
     if (newView === currentView) {
       return
     }
-    currentView.setAttribute('hidden', true)
+    currentView.setAttribute('hidden', true) // The "true" here is superfluous, but this is shorter than creating an empty attribute
     newView.removeAttribute('hidden')
   }
 
@@ -18,7 +35,6 @@ export default class extends Controller {
    * Resets form input elements. Waits a short period to ensure that inputs are not wiped before being sent to the backend
    */
   resetForm({ params }) {
-    console.log("bi")
     setTimeout(() => [...document.getElementsByClassName(params.form)].forEach(inputElement => inputElement.value = ''), 100)
   }
 }
