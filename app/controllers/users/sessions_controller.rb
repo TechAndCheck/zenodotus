4243ef8a336c748fcfae3608956f498ec490e578
oneroyalace <oneroyalace@gmail.com>
2022-01-20 16:47:37 -0500

    remove useless method

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -19,10 +19,6 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   # DELETE /resource/sign_out
-  def destroy
-    super
-    # redirect_to root_url
-  end
 
   # protected
 
