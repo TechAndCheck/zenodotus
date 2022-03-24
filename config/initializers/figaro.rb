906dbb36546b5fd7b820288ac296651461f3aa5e
Christopher Guess <cguess@gmail.com>
2022-03-24 13:57:07 -0400

    Add requirement for  environment variable

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -8,6 +8,9 @@ Figaro.require_keys("KEY_ENCRYPTION_SALT")
 Figaro.require_keys("ZORKI_SERVER_URL")
 Figaro.require_keys("ZORKI_AUTH_KEY")
 
+# The URL of the currently running server for Hypatia callbacks
+Figaro.require_keys("URL")
+
 # The URL and auth key for the external Forki gem scraper
 # Figaro.require_keys("FORKI_SERVER_URL")
 # Figaro.require_keys("FORKI_AUTH_KEY")
