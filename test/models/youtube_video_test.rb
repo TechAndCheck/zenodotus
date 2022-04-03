8a2cecae702122ab8f5e46e8a528e4abb4116e1d
oneroyalace <oneroyalace@gmail.com>
2022-04-03 15:32:23 -0400

    Fix texts

--- test/models/youtube_video_test.rb
+++ test/models/youtube_video_test.rb
@@ -4,8 +4,8 @@ require "test_helper"
 class YoutubePostTest < ActiveSupport::TestCase
   include ActiveJob::TestHelper
   def setup
-    @youtube_archiver_video = YoutubeMediaSource.extract("https://www.youtube.com/watch?v=Df7UtQTFUMQ", true)
-    @youtube_archiver_video2 = YoutubeMediaSource.extract("https://www.youtube.com/watch?v=kFFvomxcLWo", true)
+    @youtube_archiver_post = YoutubeMediaSource.extract("https://www.youtube.com/watch?v=Df7UtQTFUMQ", true)
+    @youtube_archiver_post2 = YoutubeMediaSource.extract("https://www.youtube.com/watch?v=kFFvomxcLWo", true)
   end
 
   def teardown
@@ -14,29 +14,35 @@ class YoutubePostTest < ActiveSupport::TestCase
     end
   end
 
-  test "can create youtube video" do
-    archive_item = Sources::YoutubePost.create_from_youtube_archiver_hash(@youtube_archiver_video).first
+  test "can create youtube post" do
+    archive_item = Sources::YoutubePost.create_from_youtube_archiver_hash(@youtube_archiver_post).first
     assert_not_nil archive_item
     assert_kind_of ArchiveItem, archive_item
 
-    assert_equal @youtube_archiver_video.first.title, archive_item.youtube_post.title
-    assert_equal @youtube_archiver_video.first.id, archive_item.tweet.youtube_id
-    assert_equal @youtube_archiver_video.first.id, archive_item.service_id
-    assert_equal @youtube_archiver_video.first.description, archive_item.youtube_post.description
-    assert_equal @youtube_archiver_video.first.created_at, archive_item.youtube_post.posted_at
+    assert_equal @youtube_archiver_post.first["post"]["title"], archive_item.youtube_post.title
+    assert_equal @youtube_archiver_post.first["post"]["id"], archive_item.youtube_post.youtube_id
+    assert_equal @youtube_archiver_post.first["post"]["id"], archive_item.service_id
+    assert_equal @youtube_archiver_post.first["post"]["live"], archive_item.youtube_post.live
 
-    assert_not_nil archive_item.youtube_post.channel
+    assert_not_nil archive_item.youtube_post.preview_image
+    assert_not_nil archive_item.youtube_post.author
     assert_not_nil archive_item.youtube_post.videos
   end
 
-  test "can archive YoutubePost from url" do
-    youtube_post = Sources::YoutubePost.create_from_url("https://www.youtube.com/watch?v=Df7UtQTFUMQ")
-    youtube_post_2 = Sources::YoutubePost.create_from_url("https://www.youtube.com/watch?v=kFFvomxcLWo")
+  test "can archive youtube post from url" do
+    youtube_post = Sources::YoutubePost.create_from_url!("https://www.youtube.com/watch?v=Df7UtQTFUMQ")
+    youtube_post_2 = Sources::YoutubePost.create_from_url!("https://www.youtube.com/watch?v=kFFvomxcLWo")
     assert_not_nil youtube_post
     assert_not_nil youtube_post_2
   end
 
-  test "can archive youtube video from url using ActiveJob" do
+  test "can archive youtube short" do
+    youtube_post = Sources::YoutubePost.create_from_url!("https://youtube.com/shorts/OgWNIBZfwDI?feature=share")
+    assert_not_nil youtube_post
+  end
+
+
+  test "can archive youtube post from url using ActiveJob" do
     Sources::YoutubePost.create_from_url!("https://www.youtube.com/watch?v=Df7UtQTFUMQ")
     Sources::YoutubePost.create_from_url!("https://www.youtube.com/watch?v=kFFvomxcLWo")
     perform_enqueued_jobs
@@ -54,7 +60,7 @@ class YoutubePostTest < ActiveSupport::TestCase
   end
 
   test "archiving a video creates a preview screenshot" do
-    archive_item = Sources::YoutubePost.create_from_youtube_archiver_hash(@youtube_archiver_video).first
+    archive_item = Sources::YoutubePost.create_from_youtube_archiver_hash(@youtube_archiver_post).first
     assert_not_nil archive_item.youtube_post.videos.first.video_derivatives[:preview]
   end
 end
