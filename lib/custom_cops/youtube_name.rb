40b407eaa6072a6c90234087536bbe41d647157c
Christopher Guess <cguess@gmail.com>
2022-04-08 14:37:40 -0400

    Add custom YouTube style cop
    Due to namespacing issues we have to stylize `YouTube` as `Youtube`.
    This adds a custom Rubocop cop to enforce this.
    
    To test:
    In a random file add something like `YouTubeStuff.do_something` and then
    run `rubocop` on the command line. It should flag the name. Remove the
    line and run again and it should be gone.`

---
+++ lib/custom_cops/youtube_name.rb
@@ -0,0 +1,16 @@
+# YoutubeChannelsController
+# YoutubeMediaSource
+# Sources::YoutubeChannel
+# Sources::YoutubePost
+# YoutubeMediaSourceTest
+# YoutubeChannelTest
+
+module CustomCops
+  class YoutubeName < RuboCop::Cop::Cop
+    MSG = "`YouTube` must be stylized as `Youtube`".freeze
+
+    def on_send(node)
+      add_offense(node, message: MSG) if node.receiver&.const_name&.include?("YouTube")
+    end
+  end
+end
