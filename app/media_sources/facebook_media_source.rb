2889c6ccc14aba38bba3b9d9cafd81793adcc9ab
Christopher Guess <cguess@gmail.com>
2022-04-18 12:46:36 -0400

    Equalize Force/Non-Force
    This is the same as the previous PR but without all the weird extra
    stuff built in.

--- app/media_sources/facebook_media_source.rb
+++ app/media_sources/facebook_media_source.rb
@@ -18,7 +18,7 @@ class FacebookMediaSource < MediaSource
   # @params force [Boolean] force Hypatia to not queue a request but to scrape immediately.
   #   Default: false
   # @returns [String or nil] the path of the screenshot if the screenshot was saved
-  sig { override.params(url: String, force: T::Boolean).returns(T.any(T::Boolean, T::Array[Hash])) }
+  sig { override.params(url: String, force: T::Boolean).returns(T.any(T::Boolean,  T::Hash[String, String])) }
   def self.extract(url, force = false)
     object = self.new(url)
     return object.retrieve_facebook_post! if force
@@ -82,7 +82,7 @@ class FacebookMediaSource < MediaSource
   # Scrape the page by sending it to Hypatia and forcing the server to process the job immediately. Should only be used for tests
   #
   # @return [Hash]
-  sig { returns(T::Array[Hash]) }
+  sig { returns(Hash) }
   def retrieve_facebook_post!
     scrape = Scrape.create!({ url: @url, scrape_type: :instagram })
 
@@ -96,7 +96,9 @@ class FacebookMediaSource < MediaSource
     )
 
     raise ExternalServerError, "Error: #{response.code} returned from external Forki server" unless response.code == 200
-    JSON.parse(response.body)
+    returned_data = JSON.parse(response.body)
+    returned_data["scrape_result"] = JSON.parse(returned_data["scrape_result"])
+    returned_data
   end
 end
 
--- test/models/instagram_user_test.rb
+++ test/models/instagram_user_test.rb
@@ -1,10 +1,12 @@
 require "test_helper"
 
 class InstagramUserTest < ActiveSupport::TestCase
+  @@zorki_user = nil
+
   def setup
-    @zorki_user = InstagramMediaSource.extract(
+    @@zorki_user = InstagramMediaSource.extract(
       "https://www.instagram.com/p/CQDeYPhMJLG/", true
-    ).first["post"]["user"]
+    )["scrape_result"].first["post"]["user"] if @@zorki_user.nil?
   end
 
   def teardown
@@ -14,33 +16,33 @@ class InstagramUserTest < ActiveSupport::TestCase
   end
 
   test "can create instagram user" do
-    archive_entity = Sources::InstagramUser.create_from_zorki_hash([@zorki_user]).first
+    archive_entity = Sources::InstagramUser.create_from_zorki_hash([@@zorki_user]).first
     assert_not_nil archive_entity
     assert_kind_of ArchiveEntity, archive_entity
 
-    assert_equal @zorki_user["username"], archive_entity.instagram_user.handle
-    assert_equal @zorki_user["name"], archive_entity.instagram_user.display_name
-    assert_equal @zorki_user["username"], archive_entity.service_id
-    assert_equal @zorki_user["profile_link"], archive_entity.instagram_user.url
+    assert_equal @@zorki_user["username"], archive_entity.instagram_user.handle
+    assert_equal @@zorki_user["name"], archive_entity.instagram_user.display_name
+    assert_equal @@zorki_user["username"], archive_entity.service_id
+    assert_equal @@zorki_user["profile_link"], archive_entity.instagram_user.url
 
     # I have NO idea why this isn't working using dot-style access, but it's not, so we'll keep it
     # like this for awhile
-    assert_equal @zorki_user["profile_image_url"], archive_entity.instagram_user[:profile_image_url]
-    assert_equal @zorki_user["number_of_followers"], archive_entity.instagram_user.followers_count
-    assert_equal @zorki_user["number_of_following"], archive_entity.instagram_user.following_count
+    assert_equal @@zorki_user["profile_image_url"], archive_entity.instagram_user[:profile_image_url]
+    assert_equal @@zorki_user["number_of_followers"], archive_entity.instagram_user.followers_count
+    assert_equal @@zorki_user["number_of_following"], archive_entity.instagram_user.following_count
 
     assert_not_nil archive_entity.instagram_user.profile_image
   end
 
   test "can update instagram user" do
-    archive_entity = Sources::InstagramUser.create_from_zorki_hash([@zorki_user]).first
+    archive_entity = Sources::InstagramUser.create_from_zorki_hash([@@zorki_user]).first
 
     # Set a property to something random
     archive_entity.instagram_user.update!({ followers_count: 2 })
     assert_equal archive_entity.instagram_user.followers_count, 2
     # Now try and save it again, and make sure the followers count is correct
-    archive_entity2 = Sources::InstagramUser.create_from_zorki_hash([@zorki_user]).first.instagram_user
+    archive_entity2 = Sources::InstagramUser.create_from_zorki_hash([@@zorki_user]).first.instagram_user
     assert_equal archive_entity.service_id, archive_entity2.service_id
-    assert_equal @zorki_user["number_of_followers"], archive_entity2.followers_count
+    assert_equal @@zorki_user["number_of_followers"], archive_entity2.followers_count
   end
 end
