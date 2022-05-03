91174aa9d5328b1f63c235e3a0ebfd32504c4324
Justin Reese <justin@justinreese.com>
2022-05-03 15:20:43 -0500

    Refactor Shrine initializer
    This commit organizes the Shrine initializer to make the various
    conditions more legible. Most branching logic is nested inside named
    utility methods and separated from the final configuration of the
    storage destinations.
    
    Affects #205

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,51 +1,46 @@
 # typed: ignore
 require "shrine"
 
+require "shrine/storage/file_system"
+require "shrine/storage/memory"
 require "shrine/storage/s3"
 
-if Rails.env == "production" || Figaro.env.USE_S3_DEV_TEST == "true"
-  s3_one_day_expiration = Shrine::Storage::S3.new(
-    bucket: "zenodotus-testing", # required
-    region: "us-east-1", # required
-    access_key_id: Figaro.env.AWS_ACCESS_KEY,
-    secret_access_key: Figaro.env.AWS_ACCESS_SECRET,
-  )
-
-  s3_permanent = Shrine::Storage::S3.new(
-    bucket: "zenodotus-production", # required
+# Available buckets are `zenodotus-testing` (24-hour expiration) and `zenodotus-production` (permanant)
+def make_s3_bucket(bucket_name)
+  Shrine::Storage::S3.new(
+    bucket: bucket_name, # required
     region: "us-east-1", # required
     access_key_id: Figaro.env.AWS_ACCESS_KEY,
     secret_access_key: Figaro.env.AWS_ACCESS_SECRET,
   )
 end
 
-# Development we want to be local, test ephemeral, production ???? (file for now)
-case Rails.env
-when "development"
-  require "shrine/storage/file_system"
-
-  storage = Figaro.env.USE_S3_DEV_TEST ? s3_one_day_expiration : Shrine::Storage::FileSystem.new("public", prefix: "uploads")
-  Shrine.storages = {
-    cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
-    store: storage,                                                            # permanent
-  }
-when "test"
-  require "shrine/storage/memory"
-
-  storage = Figaro.env.USE_S3_DEV_TEST ? s3_one_day_expiration : Shrine::Storage::Memory.new
-
-  Shrine.storages = {
-    cache: Shrine::Storage::Memory.new, # temporary
-    store: storage,                     # permanent
-  }
-when "production"
-  require "shrine/storage/file_system"
-
-  Shrine.storages = {
-    cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
-    store: s3_permanent,                                                       # permanent
-  }
+def make_shrine_cache
+  case Rails.env
+  when "development"
+    Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache")
+  when "test"
+    Shrine::Storage::Memory.new
+  when "production"
+    Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache")
+  end
 end
+
+def make_shrine_storage
+  case Rails.env
+  when "development"
+    Figaro.env.USE_S3_DEV_TEST == "true" ? make_s3_bucket("zenodotus-testing") : Shrine::Storage::FileSystem.new("public", prefix: "uploads")
+  when "test"
+    Figaro.env.USE_S3_DEV_TEST == "true" ? make_s3_bucket("zenodotus-testing") : Shrine::Storage::Memory.new
+  when "production"
+    make_s3_bucket("zenodotus-production")
+  end
+end
+
+Shrine.storages = {
+  cache: make_shrine_cache(),   # temporary
+  store: make_shrine_storage(), # permanent
+}
 
 Shrine.plugin :activerecord
 Shrine.plugin :cached_attachment_data # for retaining the cached file across form redisplays
