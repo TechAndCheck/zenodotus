50f9a77f36149e76f788e2c3d3de9a5e7aaa8813
Justin Reese <justin@justinreese.com>
2022-05-05 16:54:24 -0500

    Add functionality for page-specific title content
    This commits adds an application helper for generating multi-level
    titles for the page `<title>` tag, modifies the application layout to
    use it, and adds an example to our Log In page.
    
    #198

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,18 @@
 # typed: strict
 module ApplicationHelper
   include Pagy::Frontend
+
+  def make_title_tag_content(title_hierarchy = nil, opts = {
+    delimeter: "•"
+  })
+
+    title_tag_content = ""
+    if title_hierarchy.is_a? String
+      title_tag_content += title_hierarchy
+    elsif title_hierarchy.is_a? Array
+      title_tag_content += title_hierarchy.join(" #{opts[:delimeter]} ")
+    end
+
+    return (title_tag_content.present? ? "#{title_tag_content} #{opts[:delimeter]} " : "") + "Zenodotus"
+  end
 end
