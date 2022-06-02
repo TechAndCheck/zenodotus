deea421b30d72bc7ba08b60616c6446db2bed2ea
Justin Reese <justin@justinreese.com>
2022-06-02 17:45:15 -0500

    Toggle captions that are too long
    Captions longer than 320 characters will be condensed to just their
    first three lines of text, only displaying the rest when the user
    expands it.
    
    Uses WebKit’s semi-supported line-clamping syntax (supported by Firefox
    and others), but falls back to a basic max-height.
    
    Issue #219
    Issue #226

--- app/javascript/controllers/archive_controller.js
+++ app/javascript/controllers/archive_controller.js
@@ -5,11 +5,32 @@ import utc from 'dayjs/plugin/utc'
 dayjs.extend(utc)
 
 export default class extends Controller {
-    static targets = [ "timestamp" ]
+  static targets = [
+    'timestamp',
+    'captionContent',
+    'captionToggler',
+  ]
 
-    timestampTargetConnected(element) {
-        const utcTime = element.getAttribute('datetime')
-        const localTime = dayjs.utc(utcTime).local().format('D MMM YYYY [at] h:mm A')
-        element.innerText = localTime
+  static values = { captionCollapseMode: String }
+
+  timestampTargetConnected(element) {
+    const utcTime = element.getAttribute('datetime')
+    const localTime = dayjs.utc(utcTime).local().format('D MMM YYYY [at] h:mm A')
+    element.innerText = localTime
+  }
+
+  toggleCaption() {
+    switch (this.captionCollapseModeValue) {
+      case 'collapsed':
+        this.captionCollapseModeValue = 'expanded'
+        this.captionTogglerTarget.innerText = this.captionTogglerTarget.dataset.collapseLabel
+        break
+      case 'expanded':
+        this.captionCollapseModeValue = 'collapsed'
+        this.captionTogglerTarget.innerText = this.captionTogglerTarget.dataset.expandLabel
+        break
+      default:
+        console.error('archive#toggleCaption called unexpectedly.')
     }
+  }
 }
