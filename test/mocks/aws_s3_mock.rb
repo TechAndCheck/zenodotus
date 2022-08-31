5e94deb1b91c397fd41de9526983e5bc3e0dac30
oneroyalace <oneroyalace@gmail.com>
2022-08-31 11:43:21 -0400

    Update YARD method comments

--- test/mocks/aws_s3_mock.rb
+++ test/mocks/aws_s3_mock.rb
@@ -1,12 +1,14 @@
 module AwsS3Mock
   class MediaNotMockedError < StandardError; end
   class S3Mock
-    # Raises an error if the object key passed in doesn't match a file name in the test/mock_media directory
+    # Raises an error if no file name in the `test/mocks/media` folder matches the `object_key` param
+    # This requires us to add new media to our mocks directory when we use new URLs in tests
+    # But it in doing so, it prevents us from hitting live s3 in tests (so long as we apply the s3 stub in a test file)
     #
     # @!scope class
-    # @params mock_media_filenames [Array] An array of mock media file names
-    # @params bucket_name [String] The bucket prefix of the s3 object being requested
-    # @params object_key [String] The key of the s3 object being requested
+    # @param mock_media_filenames [Array] an array of mock media file names
+    # @param bucket_name [String] the bucket prefix of the s3 object being requested
+    # @param object_key [String] the key of the s3 object being requested
     # @return nil
     def self.ensure_s3_key_mocked(mock_media_filenames, bucket_name, object_key)
       unless mock_media_filenames.include?(object_key)
@@ -15,24 +17,24 @@ module AwsS3Mock
     end
 
     # Mocks the AwsS3Downloader.download_file_in_s3_received_from_hypatia method
-    # Returns a media filename in the test/mock_media directory that matches the object key parameter
+    # Returns the name of a file in the test/mocks/media directory that matches the `object_key` parameter
     #
     # @!scope class
-    # @params object_key [String] The key of the s3 object being requested
-    # @return [String] The path to the media file matching the object_key param
+    # @param object_key [String] the key of the s3 object being requested
+    # @return [String] the path to the media file matching the `object_key` param
     def self.download_file_in_s3_received_from_hypatia(object_key)
-      bucket_name = Figaro.env.AWS_S3_BUCKET_NAME_HYPATIA  # not actually needed in the mock :)
+      bucket_name = Figaro.env.AWS_S3_BUCKET_NAME_HYPATIA  # not used in the mock. We store all mocked media in one folder
       download_from_s3(object_key, bucket_name, "")
     end
 
     # Mocks the AwsS3Downloader.download_from_s3 method
     # Ensures that the we have a local media file matching the object_key param
-    # If we do, returns the corresponding filename
+    # If we do, returns the corresponding filename. If not, raises MediaNotMockederror
     #
-    # @params object_key [String] The key of the s3 object being requested
-    # @params bucket_name [String] The name of the s3 bucket the key is located in. Not used in this mock
-    # @params local_path [String] The real s3 downloader uses this param as a save path. It isn't used in this mock.
-    # @return [String] The path to the media file matching the object_key param
+    # @param object_key [String] the key of the s3 object being requested
+    # @param bucket_name [String] the name of the s3 bucket the key is located in. Not used in this mock
+    # @param local_path [String] the real s3 downloader uses this param as a save path. It isn't used in this mock.
+    # @return [String] the path to the media file matching the object_key param
     def self.download_from_s3(object_key, bucket_name, local_path)
       mock_media_directory = File.join(File.dirname(__FILE__), "media")
       mock_media_filenames = Dir.children(mock_media_directory)
--- test/mocks/hypatia_mock.rb
+++ test/mocks/hypatia_mock.rb
@@ -4,25 +4,24 @@ module HypatiaMock
   class URLNotMockedError < StandardError; end
 
   # Returns a hash mapping media URLs to mock Hypatia response bodies
-  # Loads mock response bodies from the test/mock_data directory and combines them into a single hash
+  # Loads mock response bodies from the test/mocks/data directory and combines them into a single hash
   #
   # @!scope module
-  # @params None
-  # @return a Hash mapping URLs to mock Hypatia response bodies
+  # @return [Hash] a hash mapping URLs to mock Hypatia response bodies
   def self.ingest_mock_data
     mock_data_directory = File.join(File.dirname(__FILE__), "data")
     mock_data_filenames = Dir.children(mock_data_directory)
     mock_data_hashes = parse_mock_files(mock_data_filenames, mock_data_directory)
 
-    mock_data_hashes.reduce(:merge) # combine the hashes
+    mock_data_hashes.reduce(:merge) # combine the hashes loaded from each file
   end
 
-  # Reads the contents of an array of files and returns parsed versions of them
+  # Reads the contents of an array of files and returns JSON parsed versions of them
   #
   # @!scope module
-  # @params mock_data_filenames: an array of mock data filenames
-  # @params mock_data_directory: the name of the directory mocked data files are stored in
-  # @return An array of hashes, each containing data parsed from the mock data files
+  # @param mock_data_filenames [Array<String>] an array of mock data filenames
+  # @param mock_data_directory [String] the name of the directory mocked data files are stored in
+  # @return [Array<Hash>] an array of hashes, each containing data parsed from the mock data files
   def self.parse_mock_files(mock_data_filenames, mock_data_directory)
     mock_data_filenames.map do |fn|
       file_path = "#{mock_data_directory}/#{fn}"
@@ -32,51 +31,39 @@ module HypatiaMock
     end
   end
 
-  # Returns the mock response for the input media URL
-  # The `scrape_result` field is stringified to imitate the form Hypatia returns responses in
-  # We marshal/unmarshal loaded data to avoid mutating the mock_data hash
+  # Returns the mock Hypatia response associated with `media_url`
   #
   # @!scope module
-  # @params mock_data [Hash] A hash mapping URLs to mocked response bodies
-  # @params media_url [String] The media post URL to return a mock Hypatia response for
-  # @return [Typhoeus::Response] A Typhoeus response with a body containing mocked data
+  # @param mock_data [Hash] a hash mapping URLs to mocked response bodies
+  # @param media_url [String] the media post URL to return a mock Hypatia response for
+  # @return [Typhoeus::Response] a Typhoeus response with a body containing mocked data
   def self.generate_force_response(mock_data, media_url)
     mock_body = mock_data[media_url]
-    mock_body = Marshal.load(Marshal.dump(mock_body))  # make a deep copy to avoid mutating the mock_data hash
+    mock_body = Marshal.load(Marshal.dump(mock_body))  # make a "deep copy" of mock_body to avoid mutating the mock_data hash
 
-    # Stringify response body and scrape_result attribute
+    # Stringify the mock body and the scrape_result attribute of mock_body to imitate Hypatia's JSON serialization
     mock_body["scrape_result"] = JSON.dump(mock_body["scrape_result"])
     mock_body = JSON.dump(mock_body)
 
-    Typhoeus::Response.new(body: mock_body, code: 200) # Return the appropriate mocked responses
+    Typhoeus::Response.new(body: mock_body, code: 200)
   end
 
-  # Returns a simple success response, imitating Hypatia's responsess to non-force scrape requests
+  # Returns a simple {success: true} response, imitating Hypatia's response to non-force scrape requests
   #
   # @!scope module
-  # @return [Typhoeus::Response] A Typheous response containing a queue success message
+  # @return [Typhoeus::Response] A Typhoeus response containing a queue success message
   def self.generate_nonforce_response
     success_message = { success: true }
     Typhoeus::Response.new(body: JSON.dump(success_message), code: 200)
   end
 
-
-  # Validates that the input media URL has been mocked and returns a hypatia response with mocked data
+  # Raises an error if the `mock_data` hash doesn't have a key matching `media_url`
+  # This requires us to add new data to our mock when we use new URLs in tests
+  # But it in doing so, it prevents us from hitting live Hypatia in tests
   #
   # @!scope module
-  # @params request [Typhoeus::Request] An intercepted Typhoeus request to Hypatia
-  # @params mock_data [Hash] A hash mapping URLs to mocked response bodies
-  # @return [Typhoeus::Response] A Typhoeus response with mocked data
-  # def self.load_force_response(request, mock_data)
-  #   generate_mock_response_for_url(mock_data, media_url)
-  # end
-
-  # Raises an error if the mock_data hash doesn't contain data for the input URL
-  # This error prevents us from hitting live Hypatia in tests
-  #
-  # @!scope module
-  # @params mock_data [Hash] A hash mapping URLs to mocked response bodies
-  # @params media_url [String] The media post URL to return a mock Hypatia response for
+  # @param mock_data [Hash] a hash mapping URLs to mocked response bodies
+  # @param media_url [String] the media post URL to return a mock Hypatia response for
   # @return nil
   def self.ensure_media_url_is_mocked(mock_data, media_url)
     unless mock_data.has_key?(media_url)
@@ -86,7 +73,7 @@ module HypatiaMock
 
   # Stubs Typhoeus requests to Hypatia.
   #
-  # @! scope module
+  # @!scope module
   # @return nil
   def self.construct_typhoeus_hypatia_stub
     mock_data = ingest_mock_data
