1e90e4a7aaccba42311517dd74ae340c104c7ea4
oneroyalace <oneroyalace@gmail.com>
2022-09-01 13:43:32 -0400

    Recreate scrape_callback_works test using mock

--- test/controllers/archive_controller_test.rb
+++ test/controllers/archive_controller_test.rb
@@ -47,23 +47,42 @@ class ArchiveControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "scrape callback works" do
-    # First we need to construct a callback response. Since we can't really test the callback flow we
-    # construct a mock callback. Since doing that by hard coding is actually annoying as hell we first
-    # make a forced scrape call to Hypatia, then create it from there
+    # In this test, we imitate the flow of Zenodotus processing a MediaReview JSON object sent over by FactStream
+    # In the first step of this flow, Zenodotus creates an orphaned `MediaReview` item
+    # It then sends a scrape request to Hypatia
+    #
+    # The part of the flow we test here follows:
+    # Hypatia responds to Zenodotus' scrape request with a callback, whereupon Zenodotus uses the callback response to create a new `ArchiveItem`
+    # Zenodotus then updates the orphaned `MediaReview` item with a link to the new `ArchiveItem`
 
+    # We first create an orphaned `MediaReview` item
+    test_post_url = "https://www.instagram.com/p/CBcqOkyDDH8/"
+    media_review_item = MediaReview.create!({ original_media_link: test_post_url,
+                                                   original_media_context_description: "no context",
+                                                   media_authenticity_category: "a category" })
+
+    # Next, we create a mock callback response body
     instagram_mocks_file = File.open("test/mocks/data/instagram_posts.json")
-    hypatia_mock_response = JSON.parse(instagram_mocks_file.read)["https://www.instagram.com/p/CBcqOkyDDH8/"]
+    hypatia_mock_response = JSON.parse(instagram_mocks_file.read)[test_post_url]
+    hypatia_mock_response["scrape_result"] = JSON.dump(hypatia_mock_response["scrape_result"])
     instagram_mocks_file.close
 
-    hypatia_mock_response["scrape_result"] = JSON.dump(hypatia_mock_response["scrape_result"])
-    # file.close
 
+    # We then embed the mock response body within a hash
     scrape = Scrape.create!({ url: "https://www.instagram.com/p/CBcqOkyDDH8/", scrape_type: :instagram })
     callback_response_json = { scrape_id: scrape.id, scrape_result: hypatia_mock_response["scrape_result"] }
+
+    # This hash is POSTed to Zenodotus' `/scrape_result_callback` route
+    # Zenodotus then (eventually) calls `Scrape.fulfill`, which creates an `ArchiveItem` from the Hypatia response data
+    # and links the orphaned `MediaReview` to the `ArchiveItem`.
     post scrape_result_callback_url, as: :json, params: callback_response_json
     assert_response :success
 
     scrape.reload
+    media_review_item.reload
+
+    # Check that the the MediaReview item has been updated
     assert_not_nil scrape.archive_item
+    assert_not_nil media_review_item.archive_item
   end
 end
