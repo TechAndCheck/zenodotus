fde71367a99aa3704e6318201811a810f6745e65
Justin Reese <justin@justinreese.com>
2022-09-20 13:02:06 -0500

    Remove unnecessary duckpunching of sessions
    Two separate problems going on here:
    
    1. We were recreating `SessionsController#create` unnecessarily, as the
       code in our method was exactly the same as the normal Devise method.
       Removed that.
    2. `SessionsController#new` incorrectly set the @title_tag right in the
       controller, when we can just add it in the template itself.
    
    This commit fixes both of those, and also restores the commented-out
    `destroy` method for future reference.
    
    Issue #166

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,22 +4,19 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  def new
-    @title_tag = "Log in"
-    super
-  end
+  # def new
+  #   super
+  # end
 
   # POST /resource/sign_in
-  def create
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in)
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
-    # super
-  end
+  # def create
+  #   super
+  # end
 
   # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
 
   # protected
 
