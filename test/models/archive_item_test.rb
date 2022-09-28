00ba28c9fcf3879f3c4a60eab4ad956f6e261115
Christopher Guess <cguess@gmail.com>
2022-09-28 12:46:51 -0400

    Fix a quick test issue

--- test/models/archive_item_test.rb
+++ test/models/archive_item_test.rb
@@ -13,8 +13,8 @@ class ArchiveItemTest < ActionDispatch::IntegrationTest
   end
 
   test "destroying a user resets the submitter_id of ArchiveItems it created" do
-    sign_in users(:user3)
-    Sources::Tweet.create_from_url("https://twitter.com/unsung_son/status/1470963204855578626", users(:user3), true)
+    sign_in users(:user)
+    Sources::Tweet.create_from_url! "https://twitter.com/jack/status/20", users(:user)
     assert_not_nil ArchiveItem.first.submitter
     User.destroy(users(:user3).id)
     assert_nil ArchiveItem.first.submitter
