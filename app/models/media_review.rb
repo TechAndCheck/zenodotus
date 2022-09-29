01063e33a21400b5c2fea3432362114a0ddc083e
oneroyalace <oneroyalace@gmail.com>
2022-09-29 14:58:45 -0400

    Add `MediaReview has_many ClaimReviews` relationship
    This will allows us to access a `MediaReview`'s associated claim reviews with the `media_review.claim_reviews` syntax
    Part of issue #369

--- app/models/media_review.rb
+++ app/models/media_review.rb
@@ -2,6 +2,7 @@
 
 class MediaReview < ApplicationRecord
   belongs_to :archive_item, optional: true, class_name: "ArchiveItem"
+  has_many :claim_reviews, foreign_key: :media_review_id, class_name: "ClaimReview", dependent: :destroy
 
   # All media review without an attached piece of archive
   scope :orphaned, -> { where("archive_item_id = ?", nil) }
