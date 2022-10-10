59c6499e58fde5c1f3c37de7ba3c4d8e0e9b008e
Justin Reese <justin@justinreese.com>
2022-10-10 17:54:48 -0500

    Clean up `channels/index.js`
    This removes some old require boilerplate and an attempted import of an
    unused file.
    
    Issue #290

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,6 +1,2 @@
-// Load all the channels within this directory and all subdirectories.
-// Channel files must be named *_channel.js.
-
-const channels = require.context('.', true, /_channel\.js$/)
-channels.keys().forEach(channels)
-import "./scrapes_channel"
+// Import all the channels to be used by Action Cable, e.g.:
+// import "channels/foo"
