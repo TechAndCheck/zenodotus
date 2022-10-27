ac3727f34109a2b22b443d687500c6ec93635adc
oneroyalace <oneroyalace@gmail.com>
2022-10-26 20:23:24 -0400

    Add FactCheckInsights JSON export functionality
    Issue #275

--- app/controllers/fact_check_insights_controller.rb
+++ app/controllers/fact_check_insights_controller.rb
@@ -18,21 +18,10 @@ class FactCheckInsightsController < ApplicationController
           return
         end
 
-        send_data(generate_json,
-          type: "application/json",
-          filename: "fact_check_insights_data.json"
-        )
+        send_data(generate_json, type: "application/json", filename: "fact_check_insights.json")
       end
-      format.zip do
-        unless user_signed_in? && current_user.can_access_fact_check_insights?
-          render "You do not have permission to view that resource.", status: :unauthorized
-          return
-        end
 
-        send_data(generate_csv_zips,
-          type: "application/zip",
-          filename: "fact_check_insights_data.zip"
-        )
+      format.zip do
       end
     end
   end
@@ -56,12 +45,19 @@ private
 
   sig { returns(String) }
   def generate_json
-    # TODO: Fill this out with real data. #275
-    {}.to_json
+    all_claim_reviews = ClaimReview.all.map { |claim_review| JSON.parse(claim_review.render_for_export) }
+    all_media_reviews = MediaReview.all.map { |media_review| JSON.parse(media_review.render_for_export) }
+    metadata = {
+      "retrievedAt": Time.now,
+      "claimReviewCount": all_claim_reviews.length,
+      "mediaReviewCount": all_media_reviews.length
+    }
+
+    JSON.pretty_generate({ "claimReviews": all_claim_reviews, "mediaReviews": all_media_reviews, "meta": metadata })
   end
 
   sig { returns(T::Boolean) }
-  def generate_csv_zips
+  def generate_csv_zip
     # TODO: Fill this out with real data. #275
     true
   end
