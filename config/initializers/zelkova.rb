db06d8f8eed99e49001b2f5f1acc62ec48241de8
Christopher Guess <cguess@gmail.com>
2022-11-10 15:20:36 -0500

    Update video search so it's quicker
    This updates Eikon allowing us to limit the number of searches for
    a video, while also reducing the nearness for results to count, since
    video frames will almost never line up perfectly.

--- config/initializers/zelkova.rb
+++ config/initializers/zelkova.rb
@@ -5,7 +5,7 @@ require "zelkova"
 Rails.configuration.after_initialize do
   # Build up the graph
   graph = Zelkova.graph
-  graph.radius = 6
+  graph.radius = 100
 
   ImageHash.all.each do |image_hash|
     graph.add_node(image_hash.dhash, { id: image_hash.id })
