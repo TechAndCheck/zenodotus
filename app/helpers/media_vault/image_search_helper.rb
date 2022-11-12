4d222e08f74653084c96a67b3cb7ed93f1ca3cdf
Justin Reese <justin@justinreese.com>
2022-11-11 18:23:40 -0600

    Combine text and media search into header
    This is a very large commit and you should check the issue for more
    details, but in summary it:
    
    - Combines the functionality of the `TextSearchController` and
      `ImageSearchController` into a single `SearchController`
    - Creates a search input in the header of the site that allows both text
      and media uploads
    - Enables drag-and-drop uploading to the nav search widget
    - Removes the Turbo-based search results now that we have a global
      search form (see #456 for how we might re-enable)
    
    Note that this does not unify or beautify the search *results*, just how
    searches are triggered.
    
    Issue #440

--- app/helpers/media_vault/image_search_helper.rb
+++
@@ -1,2 +0,0 @@
-module MediaVault::ImageSearchHelper
-end
--- test/helpers/search_helper.rb
+++ test/helpers/search_helper.rb
@@ -1,10 +1,10 @@
 require "test_helper"
 
-class TextSearchHelperTest < ActionView::TestCase
+class SearchHelperTest < ActionView::TestCase
   include Devise::Test::IntegrationHelpers
 
   test "text search view finder helper" do
     youtube_post = Sources::YoutubePost.create_from_url!("https://youtube.com/shorts/OgWNIBZfwDI")
-    assert_equal search_view_for_model_instance(youtube_post), "text_search_youtube_post"
+    assert_equal "text_search_youtube_post", search_view_for_model_instance(youtube_post)
   end
 end
