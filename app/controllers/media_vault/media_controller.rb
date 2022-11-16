51a31741e67f84c479717cc5b132006748d6f885
Justin Reese <justin@justinreese.com>
2022-11-16 09:33:37 -0600

    Inherit MediaController from MediaVaultController
    Issue #366

--- app/controllers/media_vault/media_controller.rb
+++ app/controllers/media_vault/media_controller.rb
@@ -1,6 +1,6 @@
 # typed: strict
 
-class MediaVault::MediaController < ApplicationController
+class MediaVault::MediaController < MediaVaultController
   sig { void }
   def show
     @archive_item = ArchiveItem.find(params[:id])
