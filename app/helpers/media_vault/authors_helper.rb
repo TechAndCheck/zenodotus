68084547524d0690338bbf29473c7be89dc28272
Justin Reese <justin@justinreese.com>
2022-11-17 16:44:57 -0600

    Style search results like other archive items
    - Style search results the way we want
    - Semantically updates “users” to “authors” in search controller
    - Removes unused views, helpers, and tests
    
    Delivers #473

--- app/helpers/media_vault/authors_helper.rb
+++ app/helpers/media_vault/authors_helper.rb
@@ -1,2 +1,7 @@
 module MediaVault::AuthorsHelper
+  # Takes an instance of a model like Sources::InstagramUser
+  # and returns a string like "instagram_user"
+  def underscore_author_model(model_instance)
+    model_instance.class.to_s.gsub("Sources::", "").underscore
+  end
 end
--- app/helpers/media_vault/search_helper.rb
+++ app/helpers/media_vault/search_helper.rb
@@ -1,9 +1,2 @@
 module MediaVault::SearchHelper
-  # Takes an instance of a model like Sources::InstagramPost
-  # and returns a string like '_instagram_post'
-  def search_view_for_model_instance(model_instance)
-    stripped_model_name = model_instance.class.to_s.gsub("Sources::", "")
-    clean_model_name = stripped_model_name.chars.map { |char| /[A-Z]/.match?(char) ? "_" + char.downcase : char }.join # replace [A-Z] with _[a-z]
-    "media_vault/text_search/search_result" + clean_model_name
-  end
 end
