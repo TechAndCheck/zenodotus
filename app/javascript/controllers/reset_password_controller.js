f3f1d0ef96f3e9af11ab2cfaded713cae2b5535c
oneroyalace <oneroyalace@gmail.com>
2023-01-09 14:52:57 -0500

    Notify users on invalid password reset If users resetting their passwords use <6 characters for their udated password or don't properly confirm their password, we notify them by surfacing Rails' activerecord errors, which we translate into messages using config/locales/en.yml

--- app/javascript/controllers/reset_password_controller.js
+++ app/javascript/controllers/reset_password_controller.js
@@ -2,15 +2,18 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   static targets = [
-    'characterLengthText',
+    'passwordLengthText',
     'passwordResetForm',
     'passwordInput',
     'passwordConfirmationInput',
   ]
 
+  connect() {
+    console.log('Hello hello', this.elemnt)
+  }
+
   onFormSubmit(event) {
     event.preventDefault()
-    console.log("help me please")
-  }  
+    console.log('help me please')
+  }
 }
-
