8203688e32863ddfc2191ccb82f9873823251518
oneroyalace <oneroyalace@gmail.com>
2023-01-12 16:45:47 -0500

    Add method/route for media metadata export
    This commit creates a route and controller method to allow exports of metadata for single items of media. In a subsequent commit, we'll link a download button on the media view page to the newly created route
    
    Issue #481

--- app/controllers/media_vault/media_controller.rb
+++ app/controllers/media_vault/media_controller.rb
@@ -5,4 +5,16 @@ class MediaVault::MediaController < MediaVaultController
   def show
     @archive_item = ArchiveItem.find(params[:id])
   end
+
+  sig { void }
+  def export_metadata
+    archive_item = ArchiveItem.includes(:media_review, archivable_item: [:author]).find(params[:id])
+
+    # Parse JSON string, extract first (and only) item from array, then convert back to a string
+    archive_item_export = JSON.parse(ArchiveItem.generate_json_for_export([archive_item])).first.to_json
+
+    send_data archive_item_export,
+      type: "application/json",
+      filename: "media_metadata.json"
+  end
 end
