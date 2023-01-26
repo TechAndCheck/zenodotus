dc2d23b23678d28b5aec5e77d687946cc77003ef
Christopher Guess <cguess@gmail.com>
2023-01-26 12:17:04 -0500

    Pretty output for metadata download
    Change the JSON output so it's human readable. Normally we could save
    size with non-pretty JSON but we're downloading a single item so it's
    mroe likely to be read by humans so we should make it easy.

--- app/controllers/media_vault/media_controller.rb
+++ app/controllers/media_vault/media_controller.rb
@@ -12,7 +12,7 @@ class MediaVault::MediaController < MediaVaultController
     archive_item = ArchiveItem.includes(:media_review, archivable_item: [:author]).find(params[:id])
 
     # Parse JSON string, extract first (and only) item from array, then convert back to a string
-    archive_item_export = JSON.parse(ArchiveItem.generate_json_for_export([archive_item])).first.to_json
+    archive_item_export = JSON.pretty_generate(JSON.parse(ArchiveItem.generate_json_for_export([archive_item])).first)
 
     send_data archive_item_export,
       type: "application/json",
