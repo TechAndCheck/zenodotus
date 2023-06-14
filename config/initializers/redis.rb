84f8553ff6e2f0c130f728952d37272d581076f6
Christopher Guess <cguess@gmail.com>
2023-06-14 15:49:25 -0400

    Redis fix for heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,1 @@
-if ENV.has_key?("REDIS_URL")
-  $redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
-end
+$redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,13 @@
-if ENV.has_key?("REDIS_URL")
-  Sidekiq.configure_server do |config|
-    config.redis = {
+Sidekiq.configure_server do |config|
+  config.redis = {
+    url: ENV["REDIS_URL"],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = {
       url: ENV["REDIS_URL"],
       ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
-    }
-  end
-
-  Sidekiq.configure_client do |config|
-    config.redis = {
-        url: ENV["REDIS_URL"],
-        ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
-    }
-  end
+  }
 end
