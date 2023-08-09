daa41373c65d0b4756ba5f08be62819487830f38
Christopher Guess <cguess@gmail.com>
2023-08-09 13:58:22 -0400

    Fix up a regression on archive.org links

--- test/models/media_review_test.rb
+++ test/models/media_review_test.rb
@@ -5,7 +5,7 @@ class MediaReviewTest < ActiveSupport::TestCase
   include ActiveJob::TestHelper
 
   def before_all
-    @@media_review_kwargs = {
+    @media_review_kwargs = {
       media_url: "https://www.foobar.com/1",
       original_media_link: "https://www.foobar.com/1_original_media_link",
       date_published: "2021-02-03",
@@ -45,7 +45,7 @@ class MediaReviewTest < ActiveSupport::TestCase
       }
     }
 
-    @@expected = {
+    @expected = {
                     "id": "d5c80c08-3f31-4e9c-93ed-e044e954e78a",
                     "@context": "https://schema.org",
                     "@type": "MediaReview",
@@ -90,15 +90,15 @@ class MediaReviewTest < ActiveSupport::TestCase
   end
 
   # A note for this test: if we change/update the output for MediaReview this will break immediately
-  # That's FINE, just fix it so that the output is generated is represented in `@@expected`.
+  # That's FINE, just fix it so that the output is generated is represented in `@expected`.
   # The only trick is to make damn sure that the code is being outputted correctly. This test is here
   # otherwise to make sure that in the course of doing something else that the output breaks.
   #
   # Basically, if you're messing around with `media_review_blueprinter` and this breaks, then just fix
   # it up manually, if you haven't been in that file something went wrong and you should fix the regression.
   test "media review blueprinter output has required characteristics" do
-    media_review = MediaReview.create!(**@@media_review_kwargs)
-    expected_copy = @@expected.deep_dup
+    media_review = MediaReview.create!(**@media_review_kwargs)
+    expected_copy = @expected.deep_dup
     expected_copy["id"] = media_review.id
 
     media_review_json = JSON.parse(media_review.render_for_export) # call blueprinter
@@ -113,6 +113,7 @@ class MediaReviewTest < ActiveSupport::TestCase
     # so we'll remove them before comparing the rest of the hash
     expected_copy["id"] = ""
     media_review_json["id"] = ""
+
     assert_equal expected_copy, media_review_json.stringify_keys
   end
 
@@ -121,7 +122,7 @@ class MediaReviewTest < ActiveSupport::TestCase
   # if there was more than one element in the array the blueprinter would break (which is actually
   # counter intuitive, but here we are), so I'm keeping it for now.
   test "can serialize mediareview with empty mediaItemAppearance array" do
-    kwargs_copy = @@media_review_kwargs.deep_dup
+    kwargs_copy = @media_review_kwargs.deep_dup
     kwargs_copy[:item_reviewed][:mediaItemAppearance] = []
 
     media_review = MediaReview.create!(**kwargs_copy)
@@ -132,17 +133,17 @@ class MediaReviewTest < ActiveSupport::TestCase
   end
 
   test "can check if orphaned" do
-    kwargs_copy = @@media_review_kwargs.deep_dup
+    kwargs_copy = @media_review_kwargs.deep_dup
 
     media_review = MediaReview.create!(**kwargs_copy)
     assert media_review.orphaned?
   end
 
   test "can get humanized media authenticity categories" do
-    media_review = MediaReview.create!(**@@media_review_kwargs)
+    media_review = MediaReview.create!(**@media_review_kwargs)
     assert_equal "Transformed", media_review.media_authenticity_category_humanized
 
-    media_review_kwargs = @@media_review_kwargs
+    media_review_kwargs = @media_review_kwargs
     media_review_kwargs[:media_authenticity_category] = [
       "DecontextualizedContent",
       "EditedOrCroppedContent",
@@ -152,7 +153,7 @@ class MediaReviewTest < ActiveSupport::TestCase
       "TransformedContent",
     ]
 
-    media_review = MediaReview.create!(**@@media_review_kwargs)
+    media_review = MediaReview.create!(**@media_review_kwargs)
 
     assert_equal("Missing Context, Edited or Cropped, Original, Satire or Parody, Staged, Transformed", media_review.media_authenticity_category_humanized)
   end
