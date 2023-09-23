7169d326eae840892860f523e817cd10e5908c29
Christopher Guess <cguess@gmail.com>
2023-09-23 11:50:46 +0200

    force a name for claim review author

--- app/models/claim_review_author.rb
+++ app/models/claim_review_author.rb
@@ -1,3 +1,8 @@
 class ClaimReviewAuthor < ApplicationRecord
   has_many :claim_reviews, dependent: false
+
+  before_save do
+    self.url = "" if self.url.nil?
+    self.name = self.url if self.name.nil?
+  end
 end
