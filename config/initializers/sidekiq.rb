580306320c48302015448255fbfda67bf477d23c
Christopher Guess <cguess@gmail.com>
2023-11-22 15:08:13 -0500

    Remove semantic logging for not playing nice with sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,22 +1,9 @@
-# debugger
-# method = Sidekiq.server? ? :configure_server : :configure_client
-# Sidekiq.public_send(method) { |cfg| cfg.logger = SemanticLogger[Sidekiq] }
-
 if ENV.has_key?("REDISCLOUD_URL")
   Sidekiq.configure_server do |config|
     config.redis = {
       url: ENV["REDISCLOUD_URL"],
       ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
     }
-
-    module SemanticLoggerSidekiqContext
-      def log(log, *args)
-        log.assign_hash(Sidekiq::Context.current)
-        super
-      end
-    end
-
-    SemanticLogger::Logger.prepend(SemanticLoggerSidekiqContext)
   end
 
   Sidekiq.configure_client do |config|
@@ -24,14 +11,5 @@ if ENV.has_key?("REDISCLOUD_URL")
         url: ENV["REDISCLOUD_URL"],
         ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
     }
-
-    module SemanticLoggerSidekiqContext
-      def log(log, *args)
-        log.assign_hash(Sidekiq::Context.current)
-        super
-      end
-    end
-
-    SemanticLogger::Logger.prepend(SemanticLoggerSidekiqContext)
   end
 end
