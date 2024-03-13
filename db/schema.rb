e91c434163d5b6d98a76273dacbdad3591d5b338
Christopher Guess <cguess@gmail.com>
2024-03-13 14:11:48 -0400

    move search to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_15_181331) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_13_165253) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "fuzzystrmatch"
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
