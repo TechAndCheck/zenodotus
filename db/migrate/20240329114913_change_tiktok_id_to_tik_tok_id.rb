fb9a652dbfdd04b59656a0972b1e2e94317be019
Christopher Guess <cguess@gmail.com>
2024-03-29 08:35:37 -0400

    YAY messed up migrations

--- db/migrate/20240329114913_change_tiktok_id_to_tik_tok_id.rb
+++ db/migrate/20240329114913_change_tiktok_id_to_tik_tok_id.rb
@@ -1,6 +1,6 @@
 class ChangeTiktokIdToTikTokId < ActiveRecord::Migration[7.0]
   def change
-    unless column_exists? :tik_tok_posts, :tiktok_id
+    if column_exists? :tik_tok_posts, :tiktok_id
       rename_column :tik_tok_posts, :tiktok_id, :tik_tok_id
     end
   end
