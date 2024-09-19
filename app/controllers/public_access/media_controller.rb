6354635373540dd034dbdd463589d09b6a163c5a
Christopher Guess <cguess@gmail.com>
2024-09-18 20:15:37 -0400

    Fix opengraph bug where if no images no preview, now use preview image of video

--- app/controllers/public_access/media_controller.rb
+++ app/controllers/public_access/media_controller.rb
@@ -9,7 +9,9 @@ class PublicAccess::MediaController < ApplicationController
     @archive_item = ArchiveItem.find_by(public_id: params[:public_id])
     description = @archive_item.normalized_attrs_for_views[:archive_item_caption].present? ? "“#{@archive_item.normalized_attrs_for_views[:archive_item_caption]}”" : "(uncaptioned media)"
     images = @archive_item.images.map { |image| image.image.download_url }
-    videos = @archive_item.videos.map { |video| video.video_derivatives[:preview].download_url }
+    images.concat @archive_item.videos.map { |video| video.video_derivatives[:preview].download_url } # Add videos as images since that's fine for previews too'
+    videos = @archive_item.videos.map { |video| video.video.download_url }
+
     @page_metadata = { title: "Archive", description: description, images: images, videos: videos }
   end
 end
