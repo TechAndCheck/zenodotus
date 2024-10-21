1f492c27977b77bde99b0e5993f6e93402ebc5b8
Christopher Guess <cguess@gmail.com>
2024-10-21 18:27:46 +0200

    fix links in scrape emails

--- test/mailers/scrape_mailer_test.rb
+++ test/mailers/scrape_mailer_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class ScrapeMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invite" do
+    # Create the email and store it for further assertions
+    email = ScrapeMailer.with(url: "https://www.example.com", user: { email: "friend@example.com" }).scrape_complete_email
+
+    # Send the email, then test that it got queued
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    # Test the body of the sent email contains what we expect it to
+    assert_equal ["no-reply@mail.factcheckinsights.com"], email.from
+    assert_equal ["friend@example.com"], email.to
+  end\
 end
