24f8e5589b1095903888c9ed67df213a2ccf8163
Christopher Guess <cguess@gmail.com>
2024-12-20 18:42:03 -0500

    Properly return 404 if public not found

--- app/controllers/public_access/media_controller.rb
+++ app/controllers/public_access/media_controller.rb
@@ -9,6 +9,12 @@ class PublicAccess::MediaController < ApplicationController
   sig { void }
   def show
     @archive_item = ArchiveItem.find_by(public_id: params[:public_id])
+
+    if @archive_item.nil?
+      render :not_found
+      return
+    end
+
     description = @archive_item.normalized_attrs_for_views[:archive_item_caption].present? ? "“#{@archive_item&.normalized_attrs_for_views[:archive_item_caption]}”" : "(uncaptioned media)"
     images = @archive_item.images.map { |image| image.image.download_url }
     images.concat @archive_item.videos.map { |video| video.video_derivatives[:preview].download_url } # Add videos as images since that's fine for previews too'
